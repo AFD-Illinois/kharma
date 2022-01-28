35b7128a53d2993bd1a35ed37eb8e27ee29c7244
Ben Prather <bprathr2@illinois.edu>
2022-01-27 21:46:00 -0600

    IL campus cluster compile/run updates

--- machines/iccl.sh
+++ machines/iccl.sh
@@ -3,7 +3,19 @@ if [[ $HOST == *".campuscluster.illinois.edu" ]]; then
   HOST_ARCH="SKX"
   DEVICE_ARCH="VOLTA70"
 
-  export NVCC_WRAPPER_DEFAULT_COMPILER='/usr/local/gcc/8.2.0/bin/g++'
+  MPI_EXE="mpirun"
+  MPI_NUM_PROCS=2
+  MPI_EXTRA_ARGS=""
+  KOKKOS_NUM_DEVICES=2
+
+  C_NATIVE="/usr/local/gcc/8.2.0/bin/gcc"
+  CXX_NATIVE="/usr/local/gcc/8.2.0/bin/g++"
+  export OMPI_CC="/usr/local/gcc/8.2.0/bin/gcc"
+  export OMPI_CXX="/usr/local/gcc/8.2.0/bin/g++"
+
+  #C_NATIVE="nvc"
+  #CXX_NATIVE="nvc++"
+
   PREFIX_PATH="$HOME/libs/hdf5-nvhpc;/usr/local/cuda/11.1"
   #PREFIX_PATH="$HOME/libs/hdf5-gcc7-cuda11-openmpi/"
   #PREFIX_PATH="$HOME/libs/hdf5-gcc7-cuda11-mvapich2/"                                                       
