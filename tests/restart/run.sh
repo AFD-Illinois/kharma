a2f0f353b252b15a074de98979e01b7ed9534d86
Ben Prather <bprathr2@illinois.edu>
2022-04-18 11:35:03 -0500

    Restart & parameter updates
    Remove ghost zones from restart files: as KHARMA now also syncs
    primitive variables, they are unnecessary and persistently buggy
    in Parthenon.
    
    Consolidate & slightly better document parameter files, make
    mhdmodes.par a walk-through example & remove confusing "implicit"
    version in favor of options in the general parfile.

--- tests/restart/run.sh
+++ tests/restart/run.sh
@@ -12,8 +12,6 @@ $KHARMADIR/run.sh -i $KHARMADIR/pars/sane.par parthenon/time/nlim=5
 
 mv torus.out0.final.phdf torus.out0.final.init.phdf
 
-python3 $KHARMADIR/scripts/fix_restart.py torus.out1.00000.rhdf
-
 $KHARMADIR/run.sh -r torus.out1.00000.rhdf parthenon/time/nlim=5
 
 mv torus.out0.final.phdf torus.out0.final.restart.phdf
