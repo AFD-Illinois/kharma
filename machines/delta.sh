c421d81238686bf9c9170747f2e5451911cb6d29
Ben Prather <bprathr2@illinois.edu>
2022-05-27 14:52:36 -0500

    Delta compile & run updates, support CPUs

--- machines/delta.sh
+++ machines/delta.sh
@@ -7,18 +7,26 @@ then
   DEVICE_ARCH=AMPERE80
 
   module load cmake
-  if [[ $ARGS == *"gcc"* ]]
+  if [[ $ARGS == *"cuda"* ]]
   then
-    module load gcc hdf5
+    if [[ $ARGS == *"gcc"* ]]
+    then
+      module load gcc hdf5
+    else
+      # Most recent nvhpc.  Keeps system MPI but uses NVHPC's?
+      module load nvhpc hdf5
+      C_NATIVE=nvc
+      CXX_NATIVE=nvc++
+    fi
   else
-    # Most recent nvhpc.  Keeps system MPI but uses NVHPC's?
-    module load nvhpc hdf5
-    C_NATIVE=nvc
-    CXX_NATIVE=nvc++
+    module load modtree/cpu gcc hdf5
   fi
 
   # MPI options
   MPI_EXE=mpirun
+  MPI_EXTRA_ARGS="--map-by ppr:4:node:pe=16"
   MPI_NUM_PROCS=4
   KOKKOS_NUM_DEVICES=4
+
+  module list
 fi
