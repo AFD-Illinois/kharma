a406d421c5c20207ea8e72e8b6261ecbdc0d6b65
Vedant Dhruv <vdhruv2@bh29.astro.illinois.edu>
2022-05-30 12:55:00 -0500

    Updated convergence scripts for conducting atmosphere

--- tests/conducting_atmosphere/check.py
+++ tests/conducting_atmosphere/check.py
@@ -11,9 +11,9 @@ if __name__=='__main__':
 	kharmadir = '/home/vdhruv2/kharma'
 	RES = [int(r) for r in sys.argv[1].split(",")]
 	NG		= 4
-	CONDUCTION = 0
+	CONDUCTION = 1
 	if CONDUCTION:
-			PRIMS = ['rho','u','u1','q']
+			PRIMS = ['rho','u','q']
 	else:
 			PRIMS = ['rho','u']
 	L1_norm = np.zeros([len(RES), len(PRIMS)])
@@ -24,7 +24,7 @@ if __name__=='__main__':
 		rho_analytic = np.loadtxt(os.path.join(kharmadir, 'kharma/prob/emhd/','conducting_atmosphere_{}_default'.format(res), 'atmosphere_soln_rho.txt'))[NG:-NG]
 		u_analytic   = np.loadtxt(os.path.join(kharmadir, 'kharma/prob/emhd/','conducting_atmosphere_{}_default'.format(res), 'atmosphere_soln_u.txt'))[NG:-NG]
 		if CONDUCTION:
-			q_analytic   = np.loadtxt(os.path.join(kharmadir, 'prob/emhd/','conducting_atmosphere_{}_default'.format(res), 'atmosphere_soln_phi.txt'))[NG:-NG]
+			q_analytic   = np.loadtxt(os.path.join(kharmadir, 'kharma/prob/emhd/','conducting_atmosphere_{}_default'.format(res), 'atmosphere_soln_phi.txt'))[NG:-NG]
 		
 		# load code data
 		dfile = h5py.File('emhd_2d_{}_end.h5'.format(res), 'r')
@@ -36,12 +36,22 @@ if __name__=='__main__':
 		
 		t   = dfile['t'][()]
 		gam = dfile['header/gam'][()]
+
+		# compute q
+		if CONDUCTION:
+			tau      = 10.
+			kappa    = 0.1
+			P        = (gam - 1.) * u
+			Theta    = P / rho
+			cs2      = (gam * P) / (rho + (gam * u))
+			chi_emhd = kappa / rho
+			q        = q_tilde * np.sqrt(chi_emhd * rho * Theta**2 / tau)
 		
 		# compute L1 norm
 		L1_norm[r,0] = np.mean(np.fabs(rho-rho_analytic[:,None]))
 		L1_norm[r,1] = np.mean(np.fabs(u-u_analytic[:,None]))
 		if CONDUCTION:
-			L1_norm[r,3] = np.mean(np.fabs(q-q_analytic[:,None]))
+			L1_norm[r,2] = np.mean(np.fabs(q-q_analytic[:,None])[1:-1])
 			
 			
 	# plotting parameters
@@ -71,7 +81,7 @@ if __name__=='__main__':
 			tracker+=1
 
 	ax.loglog([RES[0], RES[-1]], 0.1*np.asarray([float(RES[0]), float(RES[-1])])**(-2), color='k', linestyle='dashed', label='$N^{-2}$')
-	#ax.loglog([RES[0], RES[-1]], 0.001*np.asarray([float(RES[0]), float(RES[-1])])**(-1), color='k', linestyle='dotted', label='$N^{-1}$')
+	ax.loglog([RES[0], RES[-1]], 0.001*np.asarray([float(RES[0]), float(RES[-1])])**(-2), color='k', linestyle='dashed', label='$N^{-2}$')
 	plt.xscale('log', base=2)
 	ax.set_xlabel('Resolution')
 	ax.set_ylabel('L1 norm')
