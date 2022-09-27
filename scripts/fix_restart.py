3cab823c6f18217216534138bbd861bba655c078
Ben Prather <bprathr2@illinois.edu>
2022-09-27 15:13:18 -0500

    Apply restart file fixes atomically

--- scripts/fix_restart.py
+++ scripts/fix_restart.py
@@ -4,15 +4,16 @@
 # this works around a bug in Parthenon w.r.t. mesh sizes
 
 import sys
+import shutil
 import numpy as np
 import h5py
 
-outf = h5py.File(sys.argv[1], "r+")
+shutil.copyfile(sys.argv[1], sys.argv[2])
+outf = h5py.File(sys.argv[2], "r+")
 
-# Parthenon records the full size here,
-# but pretty clearly expects the size without ghost zones.
-# TODO running this script twice will cause errors
+# Parthenon records the full size here, so we amend it to exclude ghost zones
 outf['Info'].attrs.modify('MeshBlockSize',
   np.maximum(outf['Info'].attrs['MeshBlockSize'][()] - 2*outf['Info'].attrs['IncludesGhost'][()]*outf['Info'].attrs['NGhost'][()],
              np.ones_like(outf['Info'].attrs['MeshBlockSize'][()])))
+
 outf.close()
