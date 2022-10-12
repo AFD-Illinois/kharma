f6937842b81f9419def2646f759560275950ef9e
Ben Prather <bprathr2@illinois.edu>
2022-10-12 15:46:43 -0600

    Add volta compile on Darwin

--- machines/darwin.sh
+++ machines/darwin.sh
@@ -2,8 +2,8 @@
 
 # Must list which node you're compiling for:
 # ampere for AMD/NVIDIA A100 nodes
-# Later:
-# volta for x86/volta, either shared-gpu Titan V or volta-x86 queue
+# volta for x86/volta of all kinds
+# Not working yet:
 # arm-nv to compile for devkit ARM/NVIDIA nodes
 
 if [[ $HOSTNAME == "cn"* || $HOSTNAME == "darwin"* ]]; then
@@ -11,10 +11,19 @@ if [[ $HOSTNAME == "cn"* || $HOSTNAME == "darwin"* ]]; then
   module load cmake
 
   # Always our own HDF5
+  # Run ""./make.sh <usual args> hdf5" to build it
   PREFIX_PATH="$SOURCE_DIR/external/hdf5"
 
+  # These are 
   if [[ "$ARGS" == *"arm-nv"* ]]; then
-    echo "No"  
+    HOST_ARCH="ARMV81"
+    DEVICE_ARCH="AMPERE80"
+    module load nvhpc/22.7 cuda/11.7.0
+    C_NATIVE="nvc"
+    CXX_NATIVE="nvc++"
+    # New NVHPC doesn't like CUDA_HOME
+    export NVHPC_CUDA_HOME="$CUDA_HOME"
+    unset CUDA_HOME 
   elif [[ "$ARGS" == *"ampere"* ]]; then
     HOST_ARCH="ZEN3"
     DEVICE_ARCH="AMPERE80"
@@ -24,12 +33,23 @@ if [[ $HOSTNAME == "cn"* || $HOSTNAME == "darwin"* ]]; then
     # New NVHPC doesn't like CUDA_HOME
     export NVHPC_CUDA_HOME="$CUDA_HOME"
     unset CUDA_HOME
+  elif [[ "$ARGS" == *"volta"* ]]; then
+    HOST_ARCH="HSW"
+    DEVICE_ARCH="VOLTA70"
+    module load nvhpc/22.7 cuda/11.7.0
+    C_NATIVE="nvc"
+    CXX_NATIVE="nvc++"
+    # New NVHPC doesn't like CUDA_HOME
+    export NVHPC_CUDA_HOME="$CUDA_HOME"
+    unset CUDA_HOME
   else
-    echo "No"
+    echo "No target arch specified: must list a target arch for Darwin"
+    exit
   fi
 
   # Runtime
   MPI_EXE="mpirun"
-  MPI_NUM_PROCS=4
-  MPI_EXTRA_ARGS="--map-by ppr:4:node:pe=32"
+  MPI_NUM_PROCS=2
+  KOKKOS_NUM_DEVICES=2
+  MPI_EXTRA_ARGS="--map-by ppr:2:node:pe=8"
 fi
