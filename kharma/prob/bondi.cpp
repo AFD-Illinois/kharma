4b8b78ca3dcb31445fbcbae1457da5d5cef3f202
Hyerin Cho <chyerin1996@gmail.com>
2023-04-18 10:12:00 -0400

    added GIZMO 3D read-in feature

--- kharma/prob/bondi.cpp
+++ kharma/prob/bondi.cpp
@@ -118,61 +118,136 @@ TaskStatus SetBondi(MeshBlockData<Real> *rc, IndexDomain domain, bool coarse)
     if (use_gizmo && (domain != IndexDomain::outer_x1) && (domain != IndexDomain::inner_x1)) { 
         // Read the gizmo data file
         // TODO: Hyerin: maybe put this into some other function?
-        FILE *fptr;
-        fptr = fopen(datfn.c_str(),"r");
-        const int datlen=100000;
-        Real *rarr = new double[datlen];
-        Real *rhoarr = new double[datlen]; 
-        Real *Tarr = new double[datlen]; 
-        Real *vrarr = new double[datlen]; 
-        Real *Mencarr = new double[datlen]; 
-        int length=0, itemp=0;
-        while (fscanf(fptr,"%lf %lf %lf %lf %lf\n", &(rarr[itemp]), &(rhoarr[itemp]), &(Tarr[itemp]), &(vrarr[itemp]), &(Mencarr[itemp])) == 5) { // assign the read value to variable, and enter it in array
-                itemp++;
-        }
-        fclose(fptr);
-        length=itemp;
+        std::string fnstr(datfn.c_str());
+        std::string dat_type=fnstr.substr(fnstr.find_last_of(".") + 1);
+        const bool use_3d = (dat_type != "txt");
 
-        GridVector r_device("r_device", length); 
-        GridVector rho_device("rho_device", length); 
-        GridVector T_device("T_device", length); 
-        GridVector vr_device("vr_device", length); 
-        auto r_host = r_device.GetHostMirror();
-        auto rho_host = rho_device.GetHostMirror();
-        auto T_host = T_device.GetHostMirror();
-        auto vr_host = vr_device.GetHostMirror();
-        for (itemp = 0; itemp < length; itemp++) {
-            r_host(itemp) = rarr[itemp];
-            rho_host(itemp) = rhoarr[itemp];
-            T_host(itemp) = Tarr[itemp];
-            vr_host(itemp) = vrarr[itemp];
-        }
-        r_device.DeepCopy(r_host);
-        rho_device.DeepCopy(rho_host);
-        T_device.DeepCopy(T_host);
-        vr_device.DeepCopy(vr_host);
-            
-        Kokkos::fence();
-
-        pmb->par_for("gizmo_shell", kb_e.s, kb_e.e, jb_e.s, jb_e.e, ibs, ibe,
-            KOKKOS_LAMBDA_3D {
-                // same vacuum conditions at r_shell
-                GReal Xshell[GR_DIM] = {0, r_shell, 0, 0};
-                int i_sh;
-                GReal del_sh;
-                XtoindexGizmo(Xshell, r_device, length, i_sh, del_sh);
-                Real vacuum_rho, vacuum_u_over_rho, vacuum_logrho, vacuum_log_u_over_rho;
-                vacuum_rho = rho_device(i_sh)*(1.-del_sh)+rho_device(i_sh+1)*del_sh;
-                vacuum_u_over_rho = (T_device(i_sh)*(1.-del_sh)+T_device(i_sh+1)*del_sh)/(gam-1.);
-                vacuum_logrho = log10(vacuum_rho);
-                vacuum_log_u_over_rho = log10(vacuum_u_over_rho);
-
-                get_prim_gizmo_shell(G, cs, P, m_p, gam, bl, ks, r_shell, rs, vacuum_logrho, vacuum_log_u_over_rho, 
-                    r_device, rho_device, T_device, vr_device, length, k, j, i);
-                // TODO all flux
-                GRMHD::p_to_u(G, P, m_p, gam, k, j, i, U, m_u);
+        // txt file is 1D data
+        if (! use_3d) { 
+            std::cout << "GIZMO dat file is txt" << std::endl;
+            FILE *fptr;
+            fptr = fopen(datfn.c_str(),"r");
+            const int datlen=100000;
+            Real *rarr = new double[datlen];
+            Real *rhoarr = new double[datlen]; 
+            Real *Tarr = new double[datlen]; 
+            Real *vrarr = new double[datlen]; 
+            Real *Mencarr = new double[datlen]; 
+            int length=0, itemp=0;
+            while (fscanf(fptr,"%lf %lf %lf %lf %lf\n", &(rarr[itemp]), &(rhoarr[itemp]), &(Tarr[itemp]), &(vrarr[itemp]), &(Mencarr[itemp])) == 5) { // assign the read value to variable, and enter it in array
+                    itemp++;
             }
-        );
+            fclose(fptr);
+            length=itemp;
+
+            GridVector r_device("r_device", length); 
+            GridVector rho_device("rho_device", length); 
+            GridVector T_device("T_device", length); 
+            GridVector vr_device("vr_device", length); 
+            auto r_host = r_device.GetHostMirror();
+            auto rho_host = rho_device.GetHostMirror();
+            auto T_host = T_device.GetHostMirror();
+            auto vr_host = vr_device.GetHostMirror();
+            for (itemp = 0; itemp < length; itemp++) {
+                r_host(itemp) = rarr[itemp];
+                rho_host(itemp) = rhoarr[itemp];
+                T_host(itemp) = Tarr[itemp];
+                vr_host(itemp) = vrarr[itemp];
+            }
+            r_device.DeepCopy(r_host);
+            rho_device.DeepCopy(rho_host);
+            T_device.DeepCopy(T_host);
+            vr_device.DeepCopy(vr_host);
+                
+            Kokkos::fence();
+            
+            pmb->par_for("gizmo_shell", kb_e.s, kb_e.e, jb_e.s, jb_e.e, ibs, ibe,
+                KOKKOS_LAMBDA_3D {
+                    // same vacuum conditions at r_shell
+                    GReal Xshell[GR_DIM] = {0, r_shell, 0, 0};
+                    int i_sh;
+                    GReal del_sh;
+                    Real vacuum_rho, vacuum_u_over_rho, vacuum_logrho, vacuum_log_u_over_rho;
+                    XtoindexGizmo(Xshell, r_device, length, i_sh, del_sh);
+                    vacuum_rho = rho_device(i_sh)*(1.-del_sh)+rho_device(i_sh+1)*del_sh;
+                    vacuum_u_over_rho = (T_device(i_sh)*(1.-del_sh)+T_device(i_sh+1)*del_sh)/(gam-1.);
+                    vacuum_logrho = log10(vacuum_rho);
+                    vacuum_log_u_over_rho = log10(vacuum_u_over_rho);
+                
+                    get_prim_gizmo_shell(G, cs, P, m_p, gam, bl, ks, r_shell, rs, vacuum_logrho, vacuum_log_u_over_rho, 
+                                          r_device, rho_device, T_device, vr_device, length, k, j, i);
+                    // TODO all flux
+                    GRMHD::p_to_u(G, P, m_p, gam, k, j, i, U, m_u);
+                }
+            );
+        } else { 
+            // hdf5 files are 3D data
+            std::cout << "GIZMO dat file is hdf5" << std::endl;
+
+            // read from hdf5 file
+            hdf5_open(datfn.c_str());
+            hdf5_set_directory("/");
+            hsize_t length=2228224; // TODO: should be able to be retrieved from the data file
+            Real *coordarr = new double[length*3];
+            Real *rhoarr = new double[length];
+            Real *Tarr = new double[length];
+            Real *varr = new double[length*3];
+            static hsize_t fdims_vec[] = {length, 3};
+            static hsize_t fdims_scl[] = {length};
+            hsize_t fstart_vec[] = {0, 0};
+            hsize_t fstart_scl[] = {0};
+            hdf5_read_array(coordarr, "PartType0_dimless/Coordinates", 2, fdims_vec, fstart_vec,fdims_vec,fdims_vec,fstart_vec,H5T_IEEE_F64LE);
+            hdf5_read_array(rhoarr, "PartType0_dimless/Density", 1, fdims_scl, fstart_scl,fdims_scl,fdims_scl,fstart_scl,H5T_IEEE_F64LE);
+            hdf5_read_array(Tarr, "PartType0_dimless/Temperature", 1, fdims_scl, fstart_scl,fdims_scl,fdims_scl,fstart_scl,H5T_IEEE_F64LE);
+            hdf5_read_array(varr, "PartType0_dimless/Velocities", 2, fdims_vec, fstart_vec,fdims_vec,fdims_vec,fstart_vec,H5T_IEEE_F64LE);
+            hdf5_close();
+
+            // save in a device arrays
+            GridVector coord_device("coord_device", length, 3); 
+            GridScalar rho_device("rho_device", length); 
+            GridScalar T_device("rho_device", length); 
+            GridVector v_device("v_device", length, 3); 
+            auto coord_host = coord_device.GetHostMirror();
+            auto rho_host = rho_device.GetHostMirror();
+            auto T_host = T_device.GetHostMirror();
+            auto v_host = v_device.GetHostMirror();
+            int vector_file_index;
+            for (int itemp = 0; itemp < length; itemp++) {
+                for (int ltemp = 0; ltemp < 3; ltemp++) {
+                    vector_file_index = 3*itemp+ltemp;
+                    coord_host(itemp,ltemp) = coordarr[vector_file_index];
+                    v_host(itemp,ltemp) = varr[vector_file_index];
+                }
+                rho_host(itemp) = rhoarr[itemp];
+                T_host(itemp) = Tarr[itemp];
+            }
+            coord_device.DeepCopy(coord_host);
+            rho_device.DeepCopy(rho_host);
+            T_device.DeepCopy(T_host);
+            v_device.DeepCopy(v_host);
+                
+            Kokkos::fence();
+            pmb->par_for("gizmo_shell", kb_e.s, kb_e.e, jb_e.s, jb_e.e, ibs, ibe,
+                KOKKOS_LAMBDA_3D {
+                    // same vacuum conditions at r_shell
+                    GReal Xshell[GR_DIM] = {0, r_shell, 0, 0};
+                    int i_sh;
+                    GReal del_sh;
+                    Real vacuum_rho, vacuum_u_over_rho, vacuum_logrho, vacuum_log_u_over_rho;
+
+                    XtoindexGizmo3D(Xshell, coord_device, length, i_sh, del_sh);
+                    vacuum_rho = rho_device(i_sh);
+                    vacuum_u_over_rho = T_device(i_sh)/(gam-1.);
+                    vacuum_logrho = log10(vacuum_rho);
+                    vacuum_log_u_over_rho = log10(vacuum_u_over_rho);
+
+                    get_prim_gizmo_shell_3d(G, cs, P, m_p, gam, bl, ks, r_shell, rs, vacuum_logrho, vacuum_log_u_over_rho, 
+                                          coord_device, rho_device, T_device, v_device, length, k, j, i);
+                    // TODO all flux
+                    GRMHD::p_to_u(G, P, m_p, gam, k, j, i, U, m_u);
+                }
+            );
+        }
     }
     // Bondi
     else if (! (use_gizmo)) {
