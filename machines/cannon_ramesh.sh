7f316f05183025b8aa0aa0d1f56cbbabf538084a
Ben Prather <bprathr2@illinois.edu>
2023-04-25 07:40:43 -0600

    Updated Bondi run script and multizone parameters from porting to Chicoma

--- machines/cannon_ramesh.sh
+++ machines/cannon_ramesh.sh
@@ -1,69 +1,1 @@
-# Harvard Cannon
-
-#if [[ $HOST == *"rc.fas.harvard.edu" ]]; then
-if [[ $(hostname -f) == *"rc.fas.harvard.edu" ]]; then
-    echo CANNON
-    HOST_ARCH=HSW
-    EXTRA_FLAGS="-DPARTHENON_DISABLE_HDF5_COMPRESSION=ON"
-    echo $ARGS
-    echo "after printing out"
-    module purge
-    #module unload hdf5
-    #module unload Anaconda3/2020.11
-
-    if [[ "$ARGS" == *"gcc10"* ]]; then
-      # try 1: weird B cleaning phi dependence
-      module load gcc/10.2.0-fasrc01 # test
-      module load openmpi/4.1.3-fasrc03 # test
-    fi
-
-    # try 2: doesn't compile! (03/27/23), (03/29/23) but Ben says it's possible when using CUDA 12.0
-    #module load gcc/12.1.0-fasrc01
-    #module load openmpi/4.1.3-fasrc02
-
-    # try 3: 
-    #module load gcc/9.3.0-fasrc01
-    #module load openmpi/4.0.5-fasrc01
-
-    # try 4: phi dependence gone (03/29/23)
-    if [[ "$ARGS" == *"gcc08"* ]]; then
-      module load gcc/8.2.0-fasrc01
-      module load openmpi/4.1.1-fasrc02
-    fi
-
-    #module load cmake/3.17.3-fasrc01 # newer versions are usually better
-    module load cmake/3.23.2-fasrc01 # with nvhpc
-    #C_NATIVE=gcc
-    #CXX_NATIVE=g++
-  if [[ "$ARGS" == *"cuda"* ]]; then
-    #DEVICE_ARCH=VOLTA70 ## test, (old GPUs)
-    DEVICE_ARCH=AMPERE80 ## blackhole_gpu, itc_gpu
-
-    if [[ "$ARGS" == *"nvhpc"* ]]; then 
-      # use nvhpc instead (03/28/23)
-      # https://github.com/fasrc/User_Codes/blob/master/Documents/Software/Spack.md
-      source /n/holylfs05/LABS/bhi/Users/hyerincho/grmhd/spack/share/spack/setup-env.sh
-      #spack load nvhpc/tkncmer # with mpi
-      spack load nvhpc@22.7
-      spack find --loaded
-      #PREFIX_PATH=/n/holylfs05/LABS/bhi/Users/hyerincho/grmhd/spack/opt/spack/linux-centos7-haswell/gcc-4.8.5/nvhpc-22.9-tkncmerau6ssssdbw6gpgusvqy6r4grc/Linux_x86_64/22.9/comm_libs/mpi/
-      C_NATIVE=nvc #mpicc #
-      CXX_NATIVE=nvc++ #mpicxx #
-      export CXXFLAGS="-mp"
-      export CFLAGS="-mp"
-    fi
-
-    #module load cuda/11.1.0-fasrc01
-    module load cuda/11.6.2-fasrc01
-    export PATH=/n/home09/hyerincho/packages/hdf5-openmpi4.1.1:$PATH
-  else
-    module load intel/19.0.5-fasrc01
-    module load openmpi/4.0.1-fasrc01
-    export PATH=/n/home09/hyerincho/packages/hdf5-openmpi4.0.1:$PATH
-    export PATH=/n/helmod/apps/centos7/Core/gcc/9.3.0-fasrc01/bin:$PATH
-    export LIBRARY_PATH=/n/helmod/apps/centos7/Core/gcc/9.3.0-fasrc01/lib64:$LIBRARY_PATH
-    export LD_LIBRARY_PATH=/n/helmod/apps/centos7/Core/gcc/9.3.0-fasrc01/lib64:$LD_LIBRARY_PATH
-  fi
-
-fi
-
+# Harvard Cannon#if [[ $HOST == *"rc.fas.harvard.edu" ]]; thenif [[ $(hostname -f) == *"rc.fas.harvard.edu" ]]; then    echo CANNON    HOST_ARCH=HSW    EXTRA_FLAGS="-DPARTHENON_DISABLE_HDF5_COMPRESSION=ON"    echo $ARGS    echo "after printing out"    module purge    #module unload hdf5    #module unload Anaconda3/2020.11    if [[ "$ARGS" == *"gcc10"* ]]; then      # try 1: weird B cleaning phi dependence      module load gcc/10.2.0-fasrc01 # test      module load openmpi/4.1.3-fasrc03 # test    fi    # try 2: doesn't compile! (03/27/23), (03/29/23) but Ben says it's possible when using CUDA 12.0    #module load gcc/12.1.0-fasrc01    #module load openmpi/4.1.3-fasrc02    # try 3:     #module load gcc/9.3.0-fasrc01    #module load openmpi/4.0.5-fasrc01    # try 4: phi dependence gone (03/29/23)    if [[ "$ARGS" == *"gcc08"* ]]; then      module load gcc/8.2.0-fasrc01      module load openmpi/4.1.1-fasrc02    fi    #module load cmake/3.17.3-fasrc01 # newer versions are usually better    module load cmake/3.23.2-fasrc01 # with nvhpc    #C_NATIVE=gcc    #CXX_NATIVE=g++  if [[ "$ARGS" == *"cuda"* ]]; then    #DEVICE_ARCH=VOLTA70 ## test, (old GPUs)    DEVICE_ARCH=AMPERE80 ## blackhole_gpu, itc_gpu    if [[ "$ARGS" == *"nvhpc"* ]]; then       # use nvhpc instead (03/28/23)      # https://github.com/fasrc/User_Codes/blob/master/Documents/Software/Spack.md      source /n/holylfs05/LABS/bhi/Users/hyerincho/grmhd/spack/share/spack/setup-env.sh      #spack load nvhpc/tkncmer # with mpi      spack load nvhpc@22.7      spack find --loaded      #PREFIX_PATH=/n/holylfs05/LABS/bhi/Users/hyerincho/grmhd/spack/opt/spack/linux-centos7-haswell/gcc-4.8.5/nvhpc-22.9-tkncmerau6ssssdbw6gpgusvqy6r4grc/Linux_x86_64/22.9/comm_libs/mpi/      C_NATIVE=nvc #mpicc #      CXX_NATIVE=nvc++ #mpicxx #      export CXXFLAGS="-mp"      export CFLAGS="-mp"    fi    #module load cuda/11.1.0-fasrc01    module load cuda/11.6.2-fasrc01    export PATH=/n/home09/hyerincho/packages/hdf5-openmpi4.1.1:$PATH  else    module load intel/19.0.5-fasrc01    module load openmpi/4.0.1-fasrc01    export PATH=/n/home09/hyerincho/packages/hdf5-openmpi4.0.1:$PATH    export PATH=/n/helmod/apps/centos7/Core/gcc/9.3.0-fasrc01/bin:$PATH    export LIBRARY_PATH=/n/helmod/apps/centos7/Core/gcc/9.3.0-fasrc01/lib64:$LIBRARY_PATH    export LD_LIBRARY_PATH=/n/helmod/apps/centos7/Core/gcc/9.3.0-fasrc01/lib64:$LD_LIBRARY_PATH  fifi
--- tests/bondi_multizone/run.sh
+++ tests/bondi_multizone/run.sh
@@ -5,52 +5,80 @@
 
 # User specified values here
 KERR=false
-JITTER=true #false #
-ROT=true #false #
+JITTER=false #true #
+ROT=false #true #
 SUPEREXP=false #true #
+SHORT_T_OUT=true
 ROT_UPHI=0.1 # 1e-10
-DIM=3
-NZONES=7
+NZONES=8
 BASE=8
 NRUNS=300
-START_RUN=0 # if this is not 0, then update start_time, out_to_in, iteration, r_out, r_in to values that you are re-starting from
-DRTAG="bondi_multizone_041723_bondi_64^3_rot${ROT_UPHI}keprst_jit"
 
 # Set paths
-KHARMADIR=../..
-PDR="/n/holylfs05/LABS/bhi/Users/hyerincho/grmhd/" ## parent directory
-DR="${PDR}data/${DRTAG}"
-parfilename="${PDR}/kharma/pars/bondi_multizone/bondi_multizone_00000.par" # parameter file
+KHARMA_DIR=$HOME/Code/kharma-multizone
+PDR=$(pwd)/..
+DR=$(pwd)
+parfilename="bondi_multizone.par" # parameter file
+
+turn_around=$(($NZONES-1))
+
+# Read some values if we're restarting
+if [ -f start_run ]; then
+  START_RUN=$(cat start_run.var)
+  start_time=$(cat start_time.var)
+  out_to_in=$(cat out_to_in.var)
+  iteration=$(cat iteration.var)
+  r_in=$(cat r_in.var)
+  r_out=$(cat r_out.var)
+
+  last_run=$(($START_RUN - 1))
+  data_dir="${DR}/$(printf %05d $last_run)"
+  out_fn="${DR}/$(printf %05d $last_run).log"
+else
+  START_RUN=0
+  start_time=0
+  out_to_in=1
+  iteration=1 # eq : (iteration-1)*(NZONES-1)<VAR<=iteration*(NZONES-1)
+  r_in=$((${BASE}**$turn_around))
+  r_out=$((${BASE}**($turn_around+2)))
+  echo "STARTING MULTIZONE RUN: $START_RUN $start_time $out_to_in $iteration $r_in $r_out"
+fi
 
 # other values determined automatically
-turn_around=$(($NZONES-1))
-start_time=0 #100337021259 #
-out_to_in=1 # -1 #
-iteration=1 # eq : (iteration-1)*(NZONES-1)<VAR<=iteration*(NZONES-1)
-r_out=$((${BASE}**($turn_around+2))) #512 #
-r_in=$((${BASE}**$turn_around)) #8 #
 ZONE0=$((2*($NZONES-1)))
+r_b=100000
+log_u_over_rho=-5.2915149 # test same vacuum conditions as r_shell when (rs=1e2.5)
+outermost_zone=$((2*($NZONES-1)))
 
 # if the directories are not present, make them.
-if [ ! -d "${DR}" ]; then
-  mkdir "${DR}"
-fi
-if [ ! -d "${PDR}logs/${DRTAG}" ]; then
-  mkdir "${PDR}logs/${DRTAG}"
-fi
+mkdir -p "${DR}"
 
 ### Start running zone by zone
 for (( VAR=$START_RUN; VAR<$NRUNS; VAR++ ))
 do
+  # Save variables as they are when entering the loop, for restarting
+  echo $VAR > start_run.var
+  echo $start_time > start_time.var
+  echo $out_to_in > out_to_in.var
+  echo $iteration > iteration.var
+  echo $r_in > r_in.var
+  echo $r_out > r_out.var
+
   args=()
-  echo "${DRTAG} iter $iteration, $VAR : t = $start_time, r_out = $r_out, r_in = $r_in"
-  logruntime=`echo "scale=20; l($r_out)*3./2-l(1.+$r_out/100000)/2." | bc -l` # round to an integer for the free-fall time (cs^2=0.01 should be updated from the desired rs value) # GIZMO
+  echo "iter $iteration, $VAR : t = $start_time, r_out = $r_out, r_in = $r_in"
+  logruntime=`echo "scale=20; l($r_out)*3./2-l(1.+$r_out/$r_b)/2." | bc -l` # round to an integer for the free-fall time (cs^2=0.01 should be updated from the desired rs value) # GIZMO
   runtime=`echo "scale=0; e($logruntime)+1" | bc -l`
-  log_u_over_rho=-5.2915149 # test same vacuum conditions as r_shell when (rs=1e2.5)
+  if [[ $SHORT_T_OUT == "true" ]]; then
+    # use the next largest annulus' runtime
+    if [ $(($VAR % $outermost_zone)) -eq 0 ]; then
+      r_out_sm=$((${r_out}/${BASE}))
+      echo "r_out=${r_out}, but I will use next largest annulus' r_out=${r_out_sm} for the runtime"
+      logruntime=`echo "scale=20; l($r_out_sm)*3./2-l(1.+$r_out_sm/$r_b)/2." | bc -l`
+      runtime=`echo "scale=0; e($logruntime)+1" | bc -l`
+    fi
+  fi
   start_time=$(($start_time+$runtime))  
 
-  #parfilename="../../kharma/pars/bondi_multizone/bondi_multizone_$(printf %05d ${VAR}).par" # parameter file
-  
   # set problem type and cleanup
   if [ $VAR -eq 0 ]; then
     prob="bondi"
@@ -76,13 +104,12 @@ do
   # output time steps
   output0_dt=$((${runtime}/100*10))
   output1_dt=$((${runtime}/20*10))
-  #output1_dt=$((${runtime}/200*10)) # test Hyerin (02/20/23)
   output2_dt=$((${runtime}/1000*10))
   
   # dt, fname, fname_fill
   if [ $VAR -ne 0 ]; then
     # update dt from the previous run
-    tag=($( tail -n 10 ${PDR}/logs/${DRTAG}/log_multizone$(printf %05d $((${VAR}-1)))_out ))
+    tag=($( tail -n 10 $out_fn))
     dt=$(printf "%.18g" "${tag[2]:3}") # previous dt
     dt_new=$(echo "scale=14; $dt*sqrt($BASE^(-3*$out_to_in))/4" | bc -l) # new dt ## TODO: r^3/2
     if (( $(echo "$dt_new > 0.00001" |bc -l) )); then
@@ -90,11 +117,10 @@ do
     else
       dt_new=0.00001
     fi
-    fname_dir="${DR}/bondi_multizone_$(printf %05d $((${VAR}-1)))"
-    fname=$(find ${fname_dir} -type f -iname "*final.rhdf")
+    fname=$(find $data_dir -type f -iname "*final.rhdf")
     if [ $VAR -ge $NZONES ]; then
       fname_fill_num=$((2*($iteration-1)*(${NZONES}-1)-${VAR}))
-      fname_fill_dir="${DR}/bondi_multizone_$(printf %05d $fname_fill_num)"
+      fname_fill_dir="${DR}/$(printf %05d $fname_fill_num)"
       fname_fill=$(find ${fname_fill_dir} -type f -iname "*final.rhdf")
     else
       fname_fill="none"
@@ -119,7 +145,7 @@ do
       args+=(" coordinates/transform=superexp ")
       args+=(" coordinates/r_br=${r_br} coordinates/npow=2.0 coordinates/cpow=1.0 ")
       #args+=(" coordinates/r_out=${r_superexp} ")
-    #  args+=(" coordinates/r_out=${r_out} ")
+      #args+=(" coordinates/r_out=${r_out} ")
     else
       args+=(" coordinates/transform=eks ")
 
@@ -131,28 +157,22 @@ do
   
 
   # data_dir, logfiles
-  data_dir="${DR}/bondi_multizone_$(printf %05d ${VAR})"
-  out_fn="${PDR}/logs/${DRTAG}/log_multizone$(printf %05d ${VAR})_out"
-  err_fn="${PDR}/logs/${DRTAG}/log_multizone$(printf %05d ${VAR})_err"
-
-  srun --mpi=pmix ${PDR}/kharma_gcc10_kep_rst.cuda -i ${parfilename} \
-                                    parthenon/job/problem_id=$prob \
-                                    parthenon/time/tlim=${start_time} parthenon/time/nlim=-1 \
-                                    parthenon/mesh/nx1=64 parthenon/mesh/nx2=64 parthenon/mesh/nx3=64 \
-                                    parthenon/meshblock/nx1=32 parthenon/meshblock/nx2=32 parthenon/meshblock/nx3=64 \
-                                    coordinates/r_in=${r_in} coordinates/a=$spin coordinates/ext_g=false \
-                                    GRMHD/reconstruction=linear_vl \
-                                    bounds/fix_flux_pole=1 \
-                                    bondi/vacuum_logrho=-8.2014518 bondi/vacuum_log_u_over_rho=${log_u_over_rho} bondi/use_gizmo=false \
-                                    b_field/type=none b_field/solver=none b_field/bz=1e-3 \
-                                    b_field/fix_flux_x1=0 b_field/initial_cleanup=0 \
-                                    resize_restart/base=$BASE resize_restart/nzone=$NZONES resize_restart/iteration=$iteration\
-                                    parthenon/output0/dt=$output0_dt \
-                                    parthenon/output1/dt=$output1_dt \
-                                    parthenon/output2/dt=$output2_dt \
-                                    ${args[@]} \
-                                    -d ${data_dir} 1> ${out_fn} 2>${err_fn}
-                                    #coordinates/transform=mks coordinates/hslope=1  coordinates/r_out=${r_out}\
+  data_dir="${DR}/$(printf %05d ${VAR})"
+  out_fn="${DR}/$(printf %05d ${VAR}).log"
+  
+  ${KHARMA_DIR}/run.sh -i ${parfilename} \
+                       parthenon/job/problem_id=$prob \
+                       parthenon/time/tlim=${start_time} \
+                       coordinates/r_in=${r_in} coordinates/a=$spin coordinates/ext_g=false \
+                       bondi/vacuum_log_u_over_rho=${log_u_over_rho} bondi/use_gizmo=false \
+                       b_field/type=none b_field/solver=none b_field/bz=1e-3 \
+                       b_field/fix_flux_x1=false b_field/initial_cleanup=false \
+                       resize_restart/base=$BASE resize_restart/nzone=$NZONES resize_restart/iteration=$iteration\
+                       parthenon/output0/dt=$output0_dt \
+                       parthenon/output1/dt=$output1_dt \
+                       parthenon/output2/dt=$output2_dt \
+                       ${args[@]} "$@" \
+                       -d ${data_dir} 1>${out_fn} 2>&1
 
   if [ $VAR -ne 0 ]; then
     if [ $(($VAR % ($NZONES-1))) -eq 0 ]; then
