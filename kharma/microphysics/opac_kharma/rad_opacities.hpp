cbe42849eded89dbd9100c41e1e01ae78324247e
pedronaethe <pedronaethemotta@gmail.com>
2026-09-12 16:50:00 +0000

    fixed bondi test [skip ci]

--- kharma/microphysics/opac_kharma/rad_opacities.hpp
+++ kharma/microphysics/opac_kharma/rad_opacities.hpp
@@ -57,13 +57,13 @@ class RadOpac
                 // 1.0e23 to match harmrad
                 const Real kappa_a_cgs = 1.0e23 * m::pow(T_cgs, -3.5) * rho_cgs * rho_cgs;
                 // make it scale free
-                return kappa_a_cgs * units_cgs.GetLengthCGSToCode();
+                return kappa_a_cgs * units_cgs.GetLengthCodeToCGS();
             }
             case OpacityType::Transparent:
                 return 0.0;
             case OpacityType::ThermalEquilibrium: {
                 const Real rho_cgs = rho * units_cgs.GetMassDensityCodeToCGS();
-                return 0.4 * rho_cgs * units_cgs.GetLengthCGSToCode();
+                return 0.4 * rho_cgs * units_cgs.GetLengthCodeToCGS();
             }
             default: {
                 return sing_opac.PlanckMeanAbsorptionCoefficient(rho, Tg);
@@ -84,7 +84,7 @@ class RadOpac
             case OpacityType::Bondi: {
                 const Real rho_cgs = rho * units_cgs.GetMassDensityCodeToCGS();
                 const Real kappa_sc_cgs = 0.4 * rho_cgs;
-                return kappa_sc_cgs * units_cgs.GetLengthCGSToCode();
+                return kappa_sc_cgs * units_cgs.GetLengthCodeToCGS();
             }
             case OpacityType::ThermalEquilibrium:
                 return 0.0;
@@ -103,9 +103,8 @@ class RadOpac
             case OpacityType::ShocktubeConstant:
                 return 4.0 * const_sigma * (Tg * Tg * Tg * Tg);
             case OpacityType::Bondi: {
-                const Real sigma_rad = 5.670374419e-5 / pc::c /
-                    (units_cgs.GetEnergyCodeToCGS() /
-                        m::pow(units_cgs.GetTemperatureCodeToCGS(), 4.0));
+                const Real energy_density_cgs =  units_cgs.GetEnergyCodeToCGS() /m::pow(units_cgs.GetLengthCodeToCGS(), 3.0);
+                const Real sigma_rad = 5.670374419e-5 / pc::c / (energy_density_cgs/m::pow(units_cgs.GetTemperatureCodeToCGS(), 4.0));
                 return 4.0 * sigma_rad * (Tg * Tg * Tg * Tg);
             }
             case OpacityType::ThermalEquilibrium:
